1cfcc6df7c6f454747330bac80f1ce47857cb146
muhammadsolih <muhammadsolih08091011@gmail.com>
2025-08-10 11:45:13 +0500

    hammasi

--- app.js
+++ app.js
@@ -9,13 +9,16 @@
 
 // console.log(res)
 
+// -------------------------------------------------------------------------
+
+
 
 // let str = `O'zbekiston buyuk davlat !`;
 
 
 // for(let i = 0; i < str.length; i++){
 //    if(str [i] == " "){
-//        continue
+      //  continue
 //    }
 //    console.log(` ${i} : ${str[i]}`)
 // }
@@ -35,6 +38,7 @@
 // }
 
 
+// -------------------------------------------------------------------------
 
 
 // let str = `hello ozbekiston`
@@ -45,6 +49,7 @@
 //    console.log(v)
 // }
 
+// -------------------------------------------------------------------------
 
 
 
@@ -55,6 +60,7 @@
 // console.log(str.slice(17,23).toUpperCase())
 
 
+// -------------------------------------------------------------------------
 
 
 // let str = `ozbekiston buyuk davlat`
@@ -63,6 +69,7 @@
 // console.log(str.slice(11,16).toUpperCase())
 // console.log(str.slice(17,23).toUpperCase())
 
+// -------------------------------------------------------------------------
 
 
 // let ism= `muhammadsolih`
@@ -72,5 +79,91 @@
 // }
 
 
-
 // let str = 'Javascript is the best programming language. Java is not'
+
+
+
+// -------------------------------------------------------------------------
+
+// let fullName = `muhammadsolih`;
+// let birthYear = 2009;
+// let isMerried = true;
+
+// console.log(`${isMerried ? `ikki boshli` : `bir boshli`} ${fullName} hozir  ${ 2025 - birthYear} yoshda`)
+
+
+
+// -------------------------------------------------------------------------
+
+
+// let str = `ozbekiston buyuk davlat`
+
+ // console.log(str.length)  //----> uzunligini kurish;
+
+// console.log(str[0])
+// console.log(str[1])
+// console.log(str[2])
+// console.log(str[3])
+// console.log(str[4])
+// console.log(str[5])
+// console.log(str[6])
+// console.log(str[7])
+// console.log(str[8])
+// console.log(str[9])
+// console.log(str[10])
+// console.log(str[11])
+// console.log(str[12])
+// console.log(str[13])
+// console.log(str[14])
+// console.log(str[15])
+// console.log(str[16])
+// console.log(str[17])
+// console.log(str[18])
+// console.log(str[19])
+// console.log(str[20])
+// console.log(str[21])
+// console.log(str[22])
+
+// -------------------------------------------------------------------------
+
+
+// let str = `o'zbekiston buyuk davlat`
+
+// for(let i = 0; i < str.length; i++){
+//    if(str == ''){
+//       continue
+//    }
+//    console.log(`${i + 1} : ${str[i].toUpperCase()}`)
+// }
+
+
+
+// -------------------------------------------------------------------------
+
+// let doch = `ozbekiston buyuk davlat buladi`
+
+// console.log(doch [doch.length - 1])  //! ---> oxiridan bitta harif olish
+
+
+
+// console.log('a'.codePointAt())   //!--- > tagidagi raqamni kuradi
+//console.log(String.fromCharCode(65))  //! ---> sonni orqasidagi raqamni aytadi
+
+
+// -------------------------------------------------------------------------
+
+// let a = 65
+// let son = 0
+
+// for (let i = 97 ; i <= 122 ; i++){
+//    son++
+//    a++
+//    console.log(`${son} : ${String.fromCharCode(i)} --> ${String.fromCharCode(a - 1)}`)
+// }
+
+
+// -------------------------------------------------------------------------
+
+
+
+
