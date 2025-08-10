743c05a228dc59d3ca6fa3c2d1c0bc5846e3eb76
muhammadsolih <muhammadsolih08091011@gmail.com>
2025-08-10 06:36:29 +0500

    furis name

---
+++ app.js
@@ -0,0 +1,76 @@
+
+
+// let fullName  = `davlat`
+
+// let ismerried = true;
+// let birthYear = 2009;
+
+// let res = ` ${fullName} ${ismerried ? "2 boshli" : "buydoq"} hozir ${2025 - birthYear} yoshda`
+
+// console.log(res)
+
+
+// let str = `O'zbekiston buyuk davlat !`;
+
+
+// for(let i = 0; i < str.length; i++){
+//    if(str [i] == " "){
+//        continue
+//    }
+//    console.log(` ${i} : ${str[i]}`)
+// }
+
+// let a = 65
+
+// for (let i = 97; i <= 122; i++) {
+//    a++
+//    console.log(`${String.fromCharCode(i)} : ${String.fromCharCode(i).codePointAt()}`)
+// }
+
+// console.log('==========')
+
+// for (let i = 65; i <= 90; i++) {
+//    a++
+//    console.log(`${String.fromCharCode(i)} : ${String.fromCharCode(i).codePointAt()}`)
+// }
+
+
+
+
+// let str = `hello ozbekiston`
+
+// for(let i in )
+
+// for(let v of str){
+//    console.log(v)
+// }
+
+
+
+
+// let str = `ozbekiston buyuk davlat`
+
+// console.log(str.slice(0,10).toUpperCase())
+// console.log(str.slice(11,16).toUpperCase())
+// console.log(str.slice(17,23).toUpperCase())
+
+
+
+
+// let str = `ozbekiston buyuk davlat`
+
+// console.log(str.slice(0,10).toUpperCase())
+// console.log(str.slice(11,16).toUpperCase())
+// console.log(str.slice(17,23).toUpperCase())
+
+
+
+// let ism= `muhammadsolih`
+
+// for(let i = 0; i > ism.length; i++) {
+//    console.log(ism[i].charAt())
+// }
+
+
+
+// let str = 'Javascript is the best programming language. Java is not'
